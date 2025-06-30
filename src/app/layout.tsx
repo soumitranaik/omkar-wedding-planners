345ea7d087fc310e1b882439e611e92f524df424
soumitranaik <soumitra.naik99@gmail.com>
2025-06-30 20:54:23 +0530

    seo description and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,34 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Playfair_Display, Lora, Roboto} from "next/font/google";
+import { Playfair_Display, Lora, Roboto } from "next/font/google";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 const playfair = Playfair_Display({
-  subsets: ['latin'],
-  weight: ['400', '700'], // adjust weights as needed
-  variable: '--font-playfair',
-  display: 'swap',
+  subsets: ["latin"],
+  weight: ["400", "700"], // adjust weights as needed
+  variable: "--font-playfair",
+  display: "swap",
 });
 
 const lora = Lora({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-lora',
-  display: 'swap',
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-lora",
+  display: "swap",
 });
 
 const roboto = Roboto({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-roboto',
-  display: 'swap',
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
-  title: "Omkar Wedding Planners",
-  description: "Best Wedding Planners & Decorators in Goa",
+  title: "Omkar Events and Wedding Planners",
+  description: "Complete event management & wedding planning in Goa - corporate events, social celebrations, destination weddings & custom decorations. Transform any occasion perfectly.",
 };
 
 export default function RootLayout({
@@ -37,12 +37,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="light" style={{ colorScheme: 'light' }}>
+    <html lang="en" className="light" style={{ colorScheme: "light" }}>
       <body
         className={`${lora.variable} ${playfair.variable} ${roboto.variable}`}
       >
-<GoogleAnalytics />
-         <Navbar />
+        <GoogleAnalytics />
+        <Navbar />
         <div className="w-full">{children}</div>
         <Footer />
       </body>
